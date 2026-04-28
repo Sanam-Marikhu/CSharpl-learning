b25e260b0707a7a929205af3bf816d567e1f6e25
Sanam-Marikhu <sanammarikhu2002@gmail.com>
2026-04-28 15:48:48 +0545

    Add files via upload
    added day two input code

---
+++ input.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet_practice
+{
+    internal class input
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine(name);
+        }
+    }
+}
