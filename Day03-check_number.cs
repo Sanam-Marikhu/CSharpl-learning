ae66610570569eec84ae6e4e9cde07bf9454b357
Sanam-Marikhu <sanammarikhu2002@gmail.com>
2026-04-29 17:46:13 +0545

    Add files via upload
    Day three completed

---
+++ Day03-check_number.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day03_Conditions
+{
+    internal class check_number
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num < 0 && num % 2 == 0)
+            {
+                Console.WriteLine("Entered number is negative even");
+            }
+            else if (num < 0 && num % 2 != 0)
+            {
+                Console.WriteLine("Entered number is negative odd");
+            }
+            else if (num > 0 && num % 2 == 0)
+            {
+                Console.WriteLine("Entered number is postive even");
+            }
+            else if (num > 0 && num % 2 != 0)
+            {
+                Console.WriteLine("Entered number is positive odd");
+            }
+
+            else
+            {
+                Console.WriteLine("Entered number is Zero.");
+            }
+        }
+    }
+}
