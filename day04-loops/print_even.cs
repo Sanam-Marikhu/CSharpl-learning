2ecd2765ceed77d07233612d7ab1148937fbcfb3
Sanam-Marikhu <sanammarikhu2002@gmail.com>
2026-04-30 19:24:17 +0545

    Add files via upload
    Day four completed

---
+++ day04-loops/print_even.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day04_loops
+{
+    internal class print_even
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            Console.Write("Enter number up to which you want to print even numbers:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            while (i <= num)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                i++;
+            }
+
+        }
+    }
+}
---
+++ day04-loops/table.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day04_loops
+{
+    internal class table
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of which you want table:");
+            int num=Convert.ToInt32(Console.ReadLine());
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i +"x"+ num+ "="+(i*num));
+            }
+        }
+    }
+}
