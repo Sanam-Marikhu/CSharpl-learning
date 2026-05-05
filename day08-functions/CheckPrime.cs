2a16ba4c13c57ff77862520a592a3d4726aaac92
Sanam-Marikhu <sanammarikhu2002@gmail.com>
2026-05-05 15:47:24 +0545

    Add files via upload

---
+++ day08-functions/CheckPrime.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day_08
+{
+    internal class CheckPrime
+    {
+        static bool Isprime(int num)
+        {
+            if (num <= 1)
+            {
+                return false;
+            }
+            for (int i = 2; i * i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+            
+            
+                return true;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number to be cecked:");
+            int n=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Is Prime?:"+Isprime(n));
+            
+
+        }
+    }
+}
