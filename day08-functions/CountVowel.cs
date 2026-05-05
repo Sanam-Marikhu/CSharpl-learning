b2e5044d4d98acc8ad1d2131732bc67539babacc
Sanam-Marikhu <sanammarikhu2002@gmail.com>
2026-05-05 16:55:14 +0545

    Add files via upload

---
+++ day08-functions/CountVowel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day_08
+{
+    internal class CountVowel
+    {
+        
+        static void Vowelcount(string txt)
+        {
+            int count = 0;
+            if (!string.IsNullOrEmpty(txt))
+            {
+                for (int i = 0; i < txt.Length; i++)
+                {
+                    char c = char.ToLower(txt[i]);
+                    if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+                    {
+                        count++;
+                    }
+
+                }
+                Console.WriteLine("Total vowel count:" + count);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a Text:");
+            string text=Console.ReadLine();
+            Vowelcount(text);
+
+        }
+    }
+}
