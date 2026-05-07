c178cad6741d7bc2bd0a46bce8cb3de399546d03
Sanam-Marikhu <sanammarikhu2002@gmail.com>
2026-05-07 13:04:52 +0545

    Add files via upload
    Day ten Completed

---
+++ day10-searching&sorting/LinearSearch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day10
+{
+    internal class LinearSearch
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 50, 27, 11, 36, 90, 25, 0 };
+            Console.Write("Enter the number you want to search: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            bool c = false;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == num)
+                {
+                    Console.WriteLine("Found at index "+i);
+                    c = true;
+                    break;
+                }
+
+            }
+            if (c == true)
+            {
+                Console.WriteLine("Entered Number is present in list.");
+            }
+
+            else
+            {
+                Console.WriteLine("Entered number is not present in list.");
+            }
+
+        }
+    }
+
+}
