6ffec5488b3cbebf8b228f0ef0edf5982ca6d6a0
Sanam-Marikhu <sanammarikhu2002@gmail.com>
2026-05-08 16:25:38 +0545

    Add files via upload
    Day eleven completed

---
+++ day11-2dArrays/DisplayMatrix.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day11_array
+{
+    internal class DisplayMatrix
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter no of rows for matrix:");
+            int row = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter no of columns for matrix:");
+            int column = Convert.ToInt32(Console.ReadLine());
+            int[,] arr= new int[row, column];
+            for(int i = 0; i <arr.GetLength(0); i++)
+            {
+                for(int j = 0; j <arr.GetLength(1); j++)
+                {
+                    Console.Write("Element[" + (i+1) + "," + (j+1) + "]"+":");
+                    arr[i,j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("Array Elements:");
+            for(int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    Console.Write(arr[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
