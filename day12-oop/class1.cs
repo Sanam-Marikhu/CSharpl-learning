bafe28cbe3a5c3e067bd029f0c23fd86fe05fc64
Sanam-Marikhu <sanammarikhu2002@gmail.com>
2026-05-09 07:52:28 +0545

    Add files via upload
    Day 12 completed

---
+++ day12-oop/class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day12
+{
+    internal class Program
+    {
+        public class Car
+        {
+           public string brand;
+           public string model;
+        }
+        static void Main(string[] args)
+        {
+           Car c1=new Car();
+            c1.brand = "Tesla";
+            c1.model = "Teslav5";
+            Console.WriteLine("Car Brand=" + c1.brand);
+            Console.WriteLine("Car Model=" + c1.model);
+        }
+   
+
+    } 
+}
