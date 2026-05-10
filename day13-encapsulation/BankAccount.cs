c64efa5ab318ee118771b296e8fdc5970e6a5ed7
Sanam-Marikhu <sanammarikhu2002@gmail.com>
2026-05-10 12:31:24 +0545

    Add files via upload
    day 13 completed

---
+++ day13-encapsulation/BankAccount.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day13
+{
+    class BankAccount
+    {
+        private double balance;
+        public  double Balance
+        {
+            get { return balance; }
+
+            set
+            {
+                if (value >=0)
+                {
+                    balance = value;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid balance.");
+                }
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount b=new BankAccount();
+            Console.Write("Enter Balance:");
+            b.Balance = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Your Balance is:"+b.Balance);
+        }
+    }
+}
