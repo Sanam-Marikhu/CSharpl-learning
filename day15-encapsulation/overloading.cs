463ab9a6e357f63343177705dd42d4deeacc01b4
Sanam-Marikhu <sanammarikhu2002@gmail.com>
2026-05-12 13:36:54 +0545

    Add files via upload

---
+++ day15-encapsulation/overloading.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day15
+{
+    class MathOperation
+    {
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b; 
+        }
+        public double Multiply(double a, int b, int c) 
+        {
+            return a * b * c;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperation m = new MathOperation();
+            Console.WriteLine("Multiply:" + m.Multiply(2, 3));
+            Console.WriteLine("Multiply:" + m.Multiply(5.5, 1.6));
+            Console.WriteLine("Multiply:" + m.Multiply(5.2,2, 3));
+        }
+    }
+}
