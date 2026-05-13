6446061d88db09256f66572fa588d1f3cfba16b8
Sanam-Marikhu <sanammarikhu2002@gmail.com>
2026-05-13 13:19:23 +0545

    Add files via upload

---
+++ day16-abstraction/Abstract.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day16
+{
+   abstract class Vehicle
+    {
+         public abstract void start();
+        public void Display()
+        {
+            Console.WriteLine("This is abstract Parent method.");
+        }
+    } 
+    class Car : Vehicle
+    {
+         public override void start()
+        {
+            Console.WriteLine("Car Starts");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car c=new Car();
+            c.start();
+            c.Display();
+        }
+    }
+}
