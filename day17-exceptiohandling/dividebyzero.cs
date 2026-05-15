3eec5bba64a29d847bd337455a2a33274ee0a827
Sanam-Marikhu <sanammarikhu2002@gmail.com>
2026-05-15 12:29:27 +0545

    Add files via upload

---
+++ day17-exceptiohandling/dividebyzero.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter two values for division:");
+                Console.Write("Enter first values:");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter second values:");
+                int b = Convert.ToInt32(Console.ReadLine());
+                int div = a / b;
+                Console.Write("Divided value:"+div);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Cannot divide by zero");
+            }
+        }
+    }
+}
