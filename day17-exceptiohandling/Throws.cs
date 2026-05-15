da08b142a7393f472e58486c722f8348bc218881
Sanam-Marikhu <sanammarikhu2002@gmail.com>
2026-05-15 13:44:19 +0545

    Add files via upload

---
+++ day17-exceptiohandling/Throws.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day17
+{
+    internal class Throws
+    {
+        static void Main(string[] args)
+        {
+            try 
+            {
+
+                Console.Write("Enter Your Age: ");
+                int age = Convert.ToInt32(Console.ReadLine());
+                if (age < 0)
+                {
+                    throw new Exception("Age Cannot Be Negative");
+                }
+                Console.WriteLine("Age:"+age);
+            }
+            catch(Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+            }
+          
+            finally
+            {
+                Console.WriteLine("Program Ended");
+            }
+        }
+          
+    }
+}
