5d8ae246ac2ef4ad92c3b0f94b079f49afc0bbaa
Sanam-Marikhu <sanammarikhu2002@gmail.com>
2026-05-16 08:45:35 +0545

    Add files via upload

---
+++ day18-filehandling/append.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day18
+{
+    internal class append
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter("data.txt", true);
+                sw.WriteLine("I am curretly learning dotnet programming language.");
+                sw.Close();
+                Console.WriteLine("Data Appended Sucessfully.");
+                if (File.Exists("datas.txt"))
+                {
+                    StreamReader sr = new StreamReader("data.txt");
+                    string line;
+                     while((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+
+                    }
+                    sr.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
