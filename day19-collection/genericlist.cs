8abd0c24a25275f8b7324f0ba3951110076279d8
Sanam-Marikhu <sanammarikhu2002@gmail.com>
2026-05-17 12:27:06 +0545

    Add files via upload

---
+++ day19-collection/genericlist.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day19
+{
+    internal class genericlist
+    {
+        static void Main(string[] args)
+        {
+            List<int> num=new List<int>();
+            Console.WriteLine("Enter five numbers:");
+            for (int i = 0; i <5; i++)
+            {
+                Console.Write("Element" + (i+1) +":");
+                int a=Convert.ToInt32(Console.ReadLine());
+                num.Add(a);
+            }
+            Console.WriteLine("List Elements:");
+            for (int i = 0;i <num.Count; i++)
+            {
+                Console.WriteLine(num[i]);
+            }
+            num.Sort();
+            Console.WriteLine("Sorted List Elements:");
+            for (int i = 0; i < num.Count; i++)
+            {
+                Console.WriteLine(num[i]);
+            }
+
+        }
+    }
+}
