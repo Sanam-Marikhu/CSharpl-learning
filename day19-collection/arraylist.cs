c57310fd3763f34bcd27329538b9a48856624a51
Sanam-Marikhu <sanammarikhu2002@gmail.com>
2026-05-17 12:14:46 +0545

    Add files via upload

---
+++ day19-collection/arraylist.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList array = new ArrayList();
+            array.Add(1);
+            array.Add("Ram");
+            array.Add(5.2);
+            foreach(var i in array)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
