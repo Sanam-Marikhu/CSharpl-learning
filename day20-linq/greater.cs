0c4421a268195405a807b8c7ad47be1e71033a4d
Sanam-Marikhu <sanammarikhu2002@gmail.com>
2026-05-18 16:06:40 +0545

    Update greater.cs

--- day20-linq/greater.cs
+++ day20-linq/greater.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +8,22 @@ namespace day_20
 {
     internal class greater
     {
+        static void Main(string[] args)
+        {
+            int [] array = { 4, 0, 85, 69, 142, 35, -56, 85, 3, 4, 9 };
+            var arr=array.Where(x => x > 25);
+            Console.WriteLine("Array Elements: ");
+            foreach (var a in array)
+            {
+                Console.Write(a + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Numbers Greater Than 25:");
+            foreach (var e in arr)
+            {
+                Console.Write(e + " ");
+            }
+
+        }
     }
 }
