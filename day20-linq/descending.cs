65b67f128bb56f037f15f2ced97c5c536c3ac81f
Sanam-Marikhu <sanammarikhu2002@gmail.com>
2026-05-18 16:09:37 +0545

    Add files via upload

---
+++ day20-linq/descending.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day_20
+{
+    internal class descending
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { -36, 0, 7, 55, -91, 36, 258, 354, 015, 24, 85, 35, -1, 7 };
+            var sort = array.OrderByDescending(x => x);
+            Console.WriteLine("Array Elements: ");
+            foreach (var a in array)
+            {
+                Console.Write(a + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Numbers in ascending order:");
+            foreach (var e in sort)
+            {
+                Console.Write(e + " ");
+            }
+        }
+    }
+}
