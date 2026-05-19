05cbe02621e8fe720d6860df9b2372b268d9c093
Sanam-Marikhu <sanammarikhu2002@gmail.com>
2026-05-19 13:44:11 +0545

    Add files via upload

---
+++ day21-delegates/delegate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day21
+{
+    delegate void Calculation(int a, int b);
+    internal class Program
+    {
+        static void Add (int a,int b)
+        {
+            Console.WriteLine("SUM:"+( a + b));
+        }
+        static void Mul (int a,int b) 
+        {
+            Console.WriteLine("MULTIPLICATION:" + (a * b));
+        }
+        static void Sub(int a,int b)
+        {
+            Console.WriteLine("DIFFERENCE:" + (a - b));
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two numbers which you want to perform different arithmetic operations: ");
+            Console.Write("Enter First Number:");
+            int x=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Second Number:");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Calculation c = Add;
+            c += Mul;
+            c+= Sub;
+            c(x,y);
+        
+
+        }
+    }
+}
