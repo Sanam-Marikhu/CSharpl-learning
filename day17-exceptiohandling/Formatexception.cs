0e967d681c358d1d9f3c626a562b541b6aeaf5b8
Sanam-Marikhu <sanammarikhu2002@gmail.com>
2026-05-19 13:25:18 +0545

    Update Formatexception.cs

--- day17-exceptiohandling/Formatexception.cs
+++ day17-exceptiohandling/Formatexception.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace day17
 {
-    internal class Formatexception
+    internal class Program
     {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter two values for division:");
+                Console.Write("Enter first values:");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter second values:");
+                int b = Convert.ToInt32(Console.ReadLine());
+                int div = a / b;
+                Console.Write("Divided value:" + div);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Cannot divide by zero");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid Input");
+            }
+            finally
+            {
+                Console.WriteLine("Program Ended");
+            }
+        }
     }
 }
