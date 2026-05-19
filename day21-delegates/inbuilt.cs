f204b914bb0608424ca6403782b6feb5e8a694cd
Sanam-Marikhu <sanammarikhu2002@gmail.com>
2026-05-19 13:58:10 +0545

    Add files via upload

---
+++ day21-delegates/inbuilt.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day21
+{
+    internal class inbuilt
+    {
+        static void Message()
+        {
+            Console.WriteLine("Welcome Dear!");
+        }
+        static int Add(int x, int y)
+        {
+            return x + y;
+
+        }
+        static void Main(string[] args)
+        {
+            Action a = Message;
+            a();
+            Func<int, int, int> f = Add;
+            Console.WriteLine("SUM:"+f(8,9));
+        }
+    }
+}
