42a5053a504e9fe5af39dc086b923409a95165ca
Sanam-Marikhu <sanammarikhu2002@gmail.com>
2026-05-20 15:02:58 +0545

    Update Employee.cs

--- day13-encapsulation/Employee.cs
+++ day13-encapsulation/Employee.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,40 @@ using System.Threading.Tasks;
 
 namespace day13
 {
-    internal class Employee
+    class Employee
     {
+        private string name;
+        private int age;
+        public string Name { get; set; }
+        public int Age 
+        {
+            get {  return age; }
+            set
+            {
+                if (value > 0)
+                {
+                    age = value;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input!");
+                }
+            }
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e=new Employee();
+            Console.Write("Enter Name:");
+            e.Name= Console.ReadLine();
+            Console.Write("Enter Age:");
+            e.Age=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Name:"+e.Name);
+            Console.WriteLine("Age:" + e.Age);
+
+        }
     }
 }
