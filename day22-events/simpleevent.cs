19a85c1f381c83fed7f9cf33f35f79eaaafd2f8e
Sanam-Marikhu <sanammarikhu2002@gmail.com>
2026-05-20 14:58:16 +0545

    Add files via upload

---
+++ day22-events/simpleevent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day22
+{
+    delegate void Notify();
+    class Process
+    {
+        public event Notify Completed;
+        public void StartProcess()
+        {
+            Console.WriteLine("Process Started");
+            Completed();
+        }
+    }
+    internal class Program
+    {
+        static void Message()
+        {
+            Console.WriteLine("Process Completed");
+        }
+        
+        static void Main(string[] args)
+        {
+            Process p=new Process();
+            p.Completed += Message;
+            p.StartProcess();
+
+        }
+    }
+}
