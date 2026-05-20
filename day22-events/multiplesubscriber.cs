8d8c4d789cc7827ba8b99930780f70239fbb8036
Sanam-Marikhu <sanammarikhu2002@gmail.com>
2026-05-20 15:12:58 +0545

    Add files via upload

---
+++ day22-events/multiplesubscriber.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day22
+{
+    delegate void Show();
+    class Process
+    {
+        public event Show Greet;
+        public void Message()
+        {
+            Console.WriteLine("Hello,Welcome to My Code");
+            Greet?.Invoke();
+        }
+    }
+    internal class multiplesubscriber
+    {
+        static void Introduction()
+        {
+            Console.WriteLine("My name is Ram.");
+        }
+        static void Complete()
+        {
+            Console.WriteLine("Code Execution Completed");
+        }
+        static void Main(string[] args)
+        {
+            Process p=new Process();
+            p.Greet += Introduction;
+            p.Greet += Complete;
+            p.Message();
+
+
+        }
+    }
+}
