65bb9869603873da460ad9c9783cec7739adff42
Sanam-Marikhu <sanammarikhu2002@gmail.com>
2026-05-21 16:17:33 +0545

    Update count_digit.cs

--- day07_strings/count_digit.cs
+++ day07_strings/count_digit.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +8,27 @@ namespace day07
 {
     internal class count_digit
     {
+        static void Main(string[] args)
+        {
+            int digit_count = 0;
+            Console.Write("Enter text with digits: ");
+            string text=Console.ReadLine();
+            if(!string.IsNullOrEmpty(text) )
+            {
+                for (int i = 0; i < text.Length; i++)
+                {
+                    char ch = char.ToLower(text[i]);
+                    if(char.IsDigit(ch))
+                    {
+                        digit_count++;
+                    }
+                }
+                Console.WriteLine("Total no of digits in text is: "+ digit_count);
+            }
+            else
+            {
+                Console.WriteLine("No Text Entered");
+            }
+        }
     }
 }
