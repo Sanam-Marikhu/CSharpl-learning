a4fc908fb51c1f49cdf5f980b3d4f4fd99ea05d1
Sanam-Marikhu <sanammarikhu2002@gmail.com>
2026-05-21 16:16:34 +0545

    Update BubbleSort.cs

--- day10-searching&sorting/BubbleSort.cs
+++ day10-searching&sorting/BubbleSort.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +8,43 @@ namespace day10
 {
     internal class BubbleSort
     {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the size of array:");
+            int size = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[size];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("Enter element " + (i + 1) + ": ");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = 0; j < arr.Length - 1 - i; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+
+            Console.WriteLine("Array element in ascending order:");
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Array element in descending order:");
+
+            for (int i = arr.Length-1; i>=0; i--)
+            {
+                Console.Write(arr[i] + " ");
+            }
+
+        }
     }
 }
