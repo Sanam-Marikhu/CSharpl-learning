7b97546d9e5be7947b2890c2a374163efb49785f
Sanam-Marikhu <sanammarikhu2002@gmail.com>
2026-05-21 16:13:30 +0545

    Update override.cs

--- day15-encapsulation/override.cs
+++ day15-encapsulation/override.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace day15
 {
-    internal class @override
+    class Vehicle
     {
+        public string color;
+        public string name;
+        public Vehicle(string col, string n)
+        {
+            this.color = col;
+            this.name = n;
+        }
+        public virtual void Start()
+        {
+            Console.WriteLine("This is Parent class");
+            Console.WriteLine("Name:"+this.name);
+            Console.WriteLine("Color:" + this.color);
+        }
+    }
+    class Car : Vehicle 
+    {
+        public override void Start()
+        {
+            Console.WriteLine("This is child class");
+        }
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle v = new Vehicle("Red","BYD");
+            Car c=new Car();
+            c.Start();
+            v.Start();
+        }
     }
 }
