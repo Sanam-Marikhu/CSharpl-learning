685fadff7c64948e0c951bf84f463d63b1301fcb
Sanam-Marikhu <sanammarikhu2002@gmail.com>
2026-05-21 16:16:14 +0545

    Update FindSmallest.cs

--- day10-searching&sorting/FindSmallest.cs
+++ day10-searching&sorting/FindSmallest.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +8,25 @@ namespace day10
 {
     internal class FindSmallest
     {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the size of array:");
+            int size=Convert.ToInt32(Console.ReadLine());
+            int[]array=new int[size];
+            for (int i=0; i<array.Length; i++)
+            {
+                Console.Write("Enter element "+(i+1)+": ");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int small = array[0];
+            for(int i=1; i < array.Length; i++)
+            {
+                if (small > array[i])
+                {
+                    small= array[i];
+                }
+            }
+            Console.WriteLine("Smallest Element= " + small);
+        }
     }
 }
