af98034dcb98d3ae532a72ecabfb17522e0b4ff5
Sanam-Marikhu <sanammarikhu2002@gmail.com>
2026-05-22 14:11:48 +0545

    Add files via upload

---
+++ day19-collection/queue.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day19
+{
+    internal class queue
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> queue = new Queue<string>();
+            queue.Enqueue("Ram");
+            queue.Enqueue("Shyam");
+            queue.Enqueue("Hari");
+            queue.Enqueue("Gita");
+            queue.Enqueue("Sita");
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("First Element:" + queue.Peek());
+            Console.WriteLine("Removed:" + queue.Dequeue());
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
