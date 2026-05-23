0ba2ef5e2b1fe45c5e46314228cadba628b52f9e
Sanam-Marikhu <sanammarikhu2002@gmail.com>
2026-05-23 09:13:27 +0545

    Add files via upload

---
+++ day23-lambdaexpressiona&anonomousmethod/anonomousmethod.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day23
+{
+    delegate void Message();
+    internal class anonomousmethod
+    {
+        static void Main(string[] args)
+        {
+            Message m = delegate ()
+            {
+                Console.WriteLine("This is anonomous method");
+            };
+            m();
+
+        }
+    }
+}
