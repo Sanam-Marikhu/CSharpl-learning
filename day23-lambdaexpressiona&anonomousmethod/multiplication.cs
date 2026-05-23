2c19bdd3d45e88f7a306563ff15203b4765a57d3
Sanam-Marikhu <sanammarikhu2002@gmail.com>
2026-05-23 08:38:16 +0545

    Add files via upload

---
+++ day23-lambdaexpressiona&anonomousmethod/multiplication.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace day23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, int> mul = 
+                (a, b) => a * b;
+            Console.WriteLine("Multiplication:" + mul(4, 8));
+        }
+    }
+}
