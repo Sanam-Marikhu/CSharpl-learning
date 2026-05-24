94900ba72b11a54e4c7ce5bfdd54e3a9389a78c6
Sanam-Marikhu <sanammarikhu2002@gmail.com>
2026-05-24 12:17:11 +0545

    Add files via upload

---
+++ day24-multipleinheritance/simplethread.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace day24
+{
+    internal class Program
+    {
+        static void Print()
+        {
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        static void Message()
+        {
+            Console.WriteLine("Child Thread");
+        }
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(Print);
+            Thread m= new Thread(Message);
+            m.Start();
+            t.Start();
+            Console.WriteLine("Printing Numbers:");
+            Console.WriteLine("Main Thread");
+        }
+    }
+}
