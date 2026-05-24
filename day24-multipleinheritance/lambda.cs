8b21bbe58e4233d952982ea947b557e5e254ce1b
Sanam-Marikhu <sanammarikhu2002@gmail.com>
2026-05-24 12:54:13 +0545

    Add files via upload

---
+++ day24-multipleinheritance/lambda.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace day24
+{
+    internal class lambda
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(() =>
+            {
+                Console.WriteLine("This is Lambda Expression");
+            });
+            t.Start();
+
+            }
+        }
+    }
+
