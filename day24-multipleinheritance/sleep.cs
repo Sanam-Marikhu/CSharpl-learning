c38bdae65f8f2c4bdab2c79134f1400219b1b2d1
Sanam-Marikhu <sanammarikhu2002@gmail.com>
2026-05-24 12:25:16 +0545

    Add files via upload

---
+++ day24-multipleinheritance/sleep.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+
+namespace day24
+{
+    internal class sleep
+    {
+        static void Print()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(1000);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Thread t=new Thread(Print);
+            t.Start();
+            Console.WriteLine("Printing numbers after every 1 second");
+
+        }
+    }
+}
