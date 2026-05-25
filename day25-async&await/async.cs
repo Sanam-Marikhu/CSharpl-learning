6fe0cf4a47ac043024623250aa3862f37f7b5635
Sanam-Marikhu <sanammarikhu2002@gmail.com>
2026-05-25 12:35:58 +0545

    Add files via upload

---
+++ day25-async&await/async.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day25
+{
+    internal class Program
+    {
+        static async Task Message()
+        {
+            await Task.Delay(2000);
+
+            Console.WriteLine(
+                "Message after 2 seconds");
+        }
+            static async Task Main(string[] args)
+        {
+            Console.WriteLine("Program Started");
+
+            await Message();
+
+            Console.WriteLine("Program Ended");
+        }
+    }
+}
