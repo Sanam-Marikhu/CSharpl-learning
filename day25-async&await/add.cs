b8dd00f34d14bde6c610f71adb9a70b4b19eef2f
Sanam-Marikhu <sanammarikhu2002@gmail.com>
2026-05-25 12:37:16 +0545

    Add files via upload

---
+++ day25-async&await/add.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace day28
+{
+    internal class ReturnAsync
+    {
+        static async Task<int> Add()
+        {
+            await Task.Delay(1000);
+
+            return 5 + 8;
+        }
+
+        static async Task Main(string[] args)
+        {
+            int sum = await Add();
+
+            Console.WriteLine("SUM: " + sum);
+        }
+    }
+}
