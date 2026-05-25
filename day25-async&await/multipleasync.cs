f6839021a257488f40e8d0ad68849ce19cbcde0e
Sanam-Marikhu <sanammarikhu2002@gmail.com>
2026-05-25 12:38:28 +0545

    Add files via upload

---
+++ day25-async&await/multipleasync.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace day28
+{
+    internal class MultipleTasks
+    {
+        static async Task Task1()
+        {
+            await Task.Delay(1000);
+
+            Console.WriteLine("Task 1 Completed");
+        }
+
+        static async Task Task2()
+        {
+            await Task.Delay(2000);
+
+            Console.WriteLine("Task 2 Completed");
+        }
+
+        static async Task Main(string[] args)
+        {
+            Task t1 = Task1();
+
+            Task t2 = Task2();
+
+            await t1;
+            await t2;
+
+            Console.WriteLine("All Tasks Finished");
+        }
+    }
+}
