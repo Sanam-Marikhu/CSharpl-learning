1950f4d73cfcd35789c589a6b35ba1273cd52cb9
Sanam-Marikhu <sanammarikhu2002@gmail.com>
2026-05-26 15:30:20 +0545

    Add files via upload

---
+++ day20-linq/groupby.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day_20
+{
+    internal class groupby
+    {
+        static void Main(string[] args)
+        {
+            string[] names =
+            {
+                "Ram",
+                "Hari",
+                "Rita",
+                "Sita"
+            };
+
+            var group =
+                names.GroupBy(x => x[0]);
+
+            foreach (var g in group)
+            {
+                Console.WriteLine(
+                    "Group: " + g.Key);
+
+                foreach (var n in g)
+                {
+                    Console.WriteLine(n);
+                }
+            }
+        }
+    }
+}
+
