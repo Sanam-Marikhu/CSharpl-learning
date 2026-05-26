028a19ed8b3b3781eb5aeeb170f33d01ca511e05
Sanam-Marikhu <sanammarikhu2002@gmail.com>
2026-05-26 15:23:09 +0545

    Add files via upload

---
+++ day20-linq/select.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day_20
+{
+    internal class select
+    {
+        static void Main(string[] args)
+        {
+            int [] num = {1, 2, 3, 4, 5, 6};
+            var square=num.Select(x => x * x);
+            Console.WriteLine("Squared Elements:");
+            foreach(var i in square)
+            {
+                Console.WriteLine(i);
+            }
+
+
+        }
+    }
+}
