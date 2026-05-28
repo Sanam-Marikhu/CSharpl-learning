bba44846898a3b52ae61151a92ae9d447d6102f9
Sanam-Marikhu <sanammarikhu2002@gmail.com>
2026-05-28 08:52:45 +0545

    Add files via upload

---
+++ day18-filehandling/copy_delete.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day18
+{
+    internal class copy_delete
+    {
+        static void Main(string[] args)
+        {
+            File.Copy("data.txt", "backup.txt");
+            Console.WriteLine("File Copied Sucessfully");
+            string data=File.ReadAllText("backup.txt");
+            Console.WriteLine(data);
+            if (File.Exists("backup.txt"))
+            {
+                File.Delete("backup.txt");
+                Console.WriteLine("File Deleted");
+            }
+        }
+    }
+}
