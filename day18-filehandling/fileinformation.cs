2e55a60634c811d86459c8a0fb6fe62eb39cca54
Sanam-Marikhu <sanammarikhu2002@gmail.com>
2026-05-28 08:58:48 +0545

    Add files via upload

---
+++ day18-filehandling/fileinformation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day18
+{
+    internal class fileinformation
+    {
+        static void Main(string[] args)
+        {
+            FileInfo f = new FileInfo("data.txt");
+            Console.WriteLine("File Name:"+f.Name);
+            Console.WriteLine("File Size:" + f.Length);
+            Console.WriteLine("Created At:" + f.CreationTime);
+        }
+    }
+}
