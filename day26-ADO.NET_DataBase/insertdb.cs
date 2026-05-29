8dbb24e53dc2266aea543dd083d098b4b6adeb18
Sanam-Marikhu <sanammarikhu2002@gmail.com>
2026-05-29 08:47:32 +0545

    Add files via upload

---
+++ day26-ADO.NET_DataBase/insertdb.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace day26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string cs =
+            "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=School;Integrated Security=True";
+
+            SqlConnection con =
+                new SqlConnection(cs);
+
+            con.Open();
+
+            string query =
+            "INSERT INTO Student VALUES " +
+            "(3,'Shyam','Bhaktapur')," +
+            "(4,'Sita','Kathmandu')," +
+            "(5,'Gita','Lalitpur')";
+
+           
+            new SqlCommand(query, con).ExecuteNonQuery();
+
+            Console.WriteLine(
+                "Data Inserted Successfully");
+
+            con.Close();
+        }
+    }
+}
