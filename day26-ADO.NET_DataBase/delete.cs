d9a2b33283afa3953af750252e9259439ddf6dda
Sanam-Marikhu <sanammarikhu2002@gmail.com>
2026-05-29 09:07:16 +0545

    Add files via upload

---
+++ day26-ADO.NET_DataBase/delete.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day26
+{
+    internal class delete
+    {
+        static void Main(string[] args)
+        {
+            string cs = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=School;Integrated Security=True";
+            SqlConnection con=new SqlConnection(cs);
+            con.Open();
+            string query = "delete from Student where ID=1";
+            SqlCommand cmd=new SqlCommand(query, con);
+            cmd.ExecuteNonQuery();
+            Console.WriteLine("Data Deleted Sucessfully");
+            con.Close();
+
+        }
+    }
+}
