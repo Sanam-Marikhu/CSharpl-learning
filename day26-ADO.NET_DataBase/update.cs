c2d9729c16cb57580a8f624dd1ee9126b2e65eac
Sanam-Marikhu <sanammarikhu2002@gmail.com>
2026-05-29 09:01:25 +0545

    Add files via upload

---
+++ day26-ADO.NET_DataBase/update.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day26
+{
+    internal class update
+    {
+        static void Main(string[] args)
+        {
+            String cs = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=School;Integrated Security=True";
+            SqlConnection con = new SqlConnection(cs);
+            con.Open();
+            string query = "update Student set Address='Kathmandu' where Id=1";
+            SqlCommand cd = new SqlCommand(query, con);
+            cd.ExecuteNonQuery();
+            Console.WriteLine("Updated Sucesfully");
+            con.Close();
+        }
+    }
+}
